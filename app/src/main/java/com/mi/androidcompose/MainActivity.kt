0a4a8149577a1a6155a3280bc353abaa99ef24af
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-06-15 01:30:41 +0200

    columns and padding

--- app/src/main/java/com/mi/androidcompose/MainActivity.kt
+++ app/src/main/java/com/mi/androidcompose/MainActivity.kt
@@ -3,27 +3,36 @@ package com.mi.androidcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.layout.padding
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            NewsStory()
         }
     }
 
     @Composable
-    fun Greeting(name: String) {
-        Text (text = "Hello $name!")
+    fun NewsStory() {
+        // padding from outer padding
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("A day in Shark Fin Cove", modifier = Modifier.padding(16.dp))          // padding from inner item padding
+            Text("Davenport, California")
+            Text("December 2018")
+        }
     }
 
     @Preview
     @Composable
-    fun PreviewGreeting() {
-        Greeting("Android")
+    fun DefaultPreview() {
+        NewsStory()
     }
 
 }
