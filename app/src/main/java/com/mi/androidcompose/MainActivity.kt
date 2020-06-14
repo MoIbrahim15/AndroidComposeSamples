13dd8b07bec9be52943e1ca79786db656863398b
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-06-15 01:19:57 +0200

    FirstExample

---
+++ app/src/main/java/com/mi/androidcompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.mi.androidcompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text (text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android")
+    }
+
+}
