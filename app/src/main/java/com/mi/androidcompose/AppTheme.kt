d1c9373ad5203ec9820ef9cc302de601f5fe8324
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-06-18 03:22:46 +0200

    toolbar

--- app/src/main/java/com/mi/androidcompose/AppTheme.kt
+++ app/src/main/java/com/mi/androidcompose/AppTheme.kt
@@ -1,12 +1,16 @@
 package com.mi.androidcompose
 
 import androidx.compose.Composable
+import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Scaffold
+import androidx.ui.material.TopAppBar
 import androidx.ui.material.lightColorPalette
+import androidx.ui.unit.dp
 
 private val themeColors = lightColorPalette(
-    primary = Color.White,
+    primary = Color.DarkGray,
     surface = Color.DarkGray,
     onSurface = Color.White
 )
@@ -14,6 +18,14 @@ private val themeColors = lightColorPalette(
 @Composable
 fun AppTheme(content: @Composable() () -> Unit) {
     MaterialTheme(colors = themeColors) {
-        content()
+        Scaffold(
+            topAppBar = {
+                TopAppBar(
+                    title = { Text(text = "Articles") },
+                    elevation = 12.dp
+                )
+            },
+            bodyContent = {content()}
+        )
     }
 }
