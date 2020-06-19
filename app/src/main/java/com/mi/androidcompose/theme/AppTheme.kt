ee55eb763d7cacfa3cad3c203d386105789d7d18
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-06-19 22:46:47 +0200

    ModalDrawerLayout

--- app/src/main/java/com/mi/androidcompose/theme/AppTheme.kt
+++ app/src/main/java/com/mi/androidcompose/theme/AppTheme.kt
@@ -2,13 +2,15 @@ package com.mi.androidcompose.theme
 
 import android.widget.Toast
 import androidx.compose.Composable
+import androidx.compose.state
 import androidx.ui.core.ContextAmbient
 import androidx.ui.foundation.Icon
 import androidx.ui.foundation.shape.corner.CircleShape
 import androidx.ui.graphics.Color
 import androidx.ui.material.*
 import androidx.ui.material.icons.Icons
-import androidx.ui.material.icons.filled.*
+import androidx.ui.material.icons.filled.Add
+import androidx.ui.unit.dp
 import com.mi.androidcompose.views.BottomBar
 import com.mi.androidcompose.views.TopBar
 
@@ -17,31 +19,65 @@ private val themeColors = lightColorPalette(
     surface = Color.DarkGray,
     onSurface = Color.White
 )
-@Composable
-fun AppTheme(content: @Composable() () -> Unit) {
-    MaterialTheme(colors = themeColors) {
-        val context = ContextAmbient.current
 
-        Scaffold(
-            topAppBar = { TopBar() },
-            bodyContent = { content() },
-            bottomAppBar = { fabConfiguration ->
-                BottomBar(
-                    fabConfiguration
-                )
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    contentColor = Color.White,
-                    backgroundColor = Color.DarkGray,
-                    icon = { Icon(Icons.Filled.Add) },
-                    shape = CircleShape,
-                    onClick = {
-                        Toast.makeText(context, "FAB", Toast.LENGTH_SHORT).show()
-                    }
-                )
-            },
-            floatingActionButtonPosition = Scaffold.FabPosition.CenterDocked
-        )
+@Composable
+fun AppTheme(featureContent: @Composable() () -> Unit) {
+    MaterialTheme(colors = themeColors) {
+        AppDrawer(featureContent)
     }
+}
+
+@Composable
+fun AppDrawer(featureContent: @Composable() () -> Unit) {
+    val (state, onStateChange) = state { DrawerState.Closed }
+    ModalDrawerLayout(
+        drawerState = state,
+        onStateChange = onStateChange,
+        drawerContent = { DrawerContent(onStateChange = onStateChange) },
+        bodyContent = {
+            AppContent(
+                content = featureContent,
+                onStateChange = onStateChange
+            )
+        },
+        drawerElevation = 8.dp,
+        drawerShape = MaterialTheme.shapes.large
+    )
+}
+
+@Composable
+fun DrawerContent(onStateChange: (DrawerState) -> Unit) {
+    //todo
+//    OutlinedButton(onClick = { Log.v("Test", "Test33333") }) {
+//        Text("Outlined Button")
+//    }
+}
+
+@Composable
+fun AppContent(
+    content: @Composable() () -> Unit,
+    onStateChange: (DrawerState) -> Unit
+) {
+    val context = ContextAmbient.current
+    Scaffold(
+        topAppBar = { TopBar(onStateChange) },
+        bottomAppBar = { fabConfiguration ->
+            BottomBar(
+                fabConfiguration
+            )
+        },
+        bodyContent = { content() },
+        floatingActionButton = {
+            FloatingActionButton(
+                contentColor = Color.White,
+                backgroundColor = Color.DarkGray,
+                icon = { Icon(Icons.Filled.Add) },
+                shape = CircleShape,
+                onClick = {
+                    Toast.makeText(context, "FAB", Toast.LENGTH_SHORT).show()
+                }
+            )
+        },
+        floatingActionButtonPosition = Scaffold.FabPosition.CenterDocked
+    )
 }
--- app/src/main/java/com/mi/androidcompose/views/TopBar.kt
+++ app/src/main/java/com/mi/androidcompose/views/TopBar.kt
@@ -1,14 +1,34 @@
 package com.mi.androidcompose.views
 
 import androidx.compose.Composable
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
+import androidx.ui.material.DrawerState
+import androidx.ui.material.IconButton
 import androidx.ui.material.TopAppBar
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Info
+import androidx.ui.material.icons.filled.Menu
 import androidx.ui.unit.dp
 
 @Composable
-fun TopBar() {
+fun TopBar(
+    onStateChange: (DrawerState) -> Unit
+) {
     TopAppBar(
         title = { Text(text = "Articles") },
+        navigationIcon = {
+            IconButton(onClick = {
+                onStateChange(DrawerState.Opened)
+            }) {
+                Icon(Icons.Filled.Menu)
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Info)
+            }
+        },
         elevation = 12.dp
     )
 }
